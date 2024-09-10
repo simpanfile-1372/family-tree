21ab6da2f93b446a50075ef97382b46c7d8e08ea
simpanfile-1372 <simpanfilee4@gmail.com>
2024-09-10 13:13:51 +0700

    Update and rename family-data.js to familyData.js

---
+++ familyData.js
@@ -0,0 +1,338 @@
+[
+    {
+      ancestor: "Dalima",
+      gender_ancestor: "female",
+      descendant: "Dahlin",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Dalima",
+      gender_ancestor: "female",
+      descendant: "Rosna",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Dalima",
+      gender_ancestor: "female",
+      descendant: "Rosmi",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Dalima",
+      gender_ancestor: "female",
+      descendant: "Warni",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Dalima",
+      gender_ancestor: "female",
+      descendant: "Dahniar",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Dalima",
+      gender_ancestor: "female",
+      descendant: "Mardani",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Dalima",
+      gender_ancestor: "female",
+      descendant: "Jawanis",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Rosna",
+      gender_ancestor: "female",
+      descendant: "Firdaus",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Rosna",
+      gender_ancestor: "female",
+      descendant: "Yetri Yenti",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Rosna",
+      gender_ancestor: "female",
+      descendant: "Maiyasni",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Rosmi",
+      gender_ancestor: "female",
+      descendant: "Refmainur",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Rosmi",
+      gender_ancestor: "female",
+      descendant: "Faridawati",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Rosmi",
+      gender_ancestor: "female",
+      descendant: "Armayuni",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Rosmi",
+      gender_ancestor: "female",
+      descendant: "Delfitri",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Warni",
+      gender_ancestor: "female",
+      descendant: "Lizwardi",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Warni",
+      gender_ancestor: "female",
+      descendant: "Syafridon",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Warni",
+      gender_ancestor: "female",
+      descendant: "Feri",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Warni",
+      gender_ancestor: "female",
+      descendant: "Hendri",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Warni",
+      gender_ancestor: "female",
+      descendant: "Fictor",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Dahniar",
+      gender_ancestor: "female",
+      descendant: "Hardi Widhasmara",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Dahniar",
+      gender_ancestor: "female",
+      descendant: "Esti Setyorini",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Dahniar",
+      gender_ancestor: "female",
+      descendant: "Atika Febrianti",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Mardani",
+      gender_ancestor: "female",
+      descendant: "Fadliah",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Mardani",
+      gender_ancestor: "female",
+      descendant: "Nursakina",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Mardani",
+      gender_ancestor: "female",
+      descendant: "M. Arkom",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Mardani",
+      gender_ancestor: "female",
+      descendant: "Memen",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Mardani",
+      gender_ancestor: "female",
+      descendant: "Idul",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Jawanis",
+      gender_ancestor: "female",
+      descendant: "Jenny Wari Jazz",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Jawanis",
+      gender_ancestor: "female",
+      descendant: "Joan Rama Jazz",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Jawanis",
+      gender_ancestor: "female",
+      descendant: "Rayhan Alim Jazz",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Yetri Yenti",
+      gender_ancestor: "female",
+      descendant: "Meza",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Yetri Yenti",
+      gender_ancestor: "female",
+      descendant: "Niko",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Yetri Yenti",
+      gender_ancestor: "female",
+      descendant: "Wawan",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Maiyasni",
+      gender_ancestor: "female",
+      descendant: "Sonia Amelia",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Maiyasni",
+      gender_ancestor: "female",
+      descendant: "M. Adithya",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Refmainur",
+      gender_ancestor: "female",
+      descendant: "Rada Ayu Pareza",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Refmainur",
+      gender_ancestor: "female",
+      descendant: "Rafy Dwi Pareza",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Faridawati",
+      gender_ancestor: "female",
+      descendant: "Maylani Usfa Putri",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Faridawati",
+      gender_ancestor: "female",
+      descendant: "Mona Ratuliu",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Armayuni",
+      gender_ancestor: "female",
+      descendant: "Kevin Salsabil Arlandy",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Armayuni",
+      gender_ancestor: "female",
+      descendant: "Kiara Dwi Arlandy",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Delfitri",
+      gender_ancestor: "female",
+      descendant: "Imam Ashari Chan",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Delfitri",
+      gender_ancestor: "female",
+      descendant: "Ibnu Dwi Putra Chan",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Delfitri",
+      gender_ancestor: "female",
+      descendant: "Indira Chantika Putri",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Esti Setyorini",
+      gender_ancestor: "female",
+      descendant: "Inara Alaq Ikrar",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Atika Febrianti",
+      gender_ancestor: "female",
+      descendant: "Yumna Fattin Balqis",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Atika Febrianti",
+      gender_ancestor: "female",
+      descendant: "Al-Umar Zaini",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Atika Febrianti",
+      gender_ancestor: "female",
+      descendant: "Rayya Avicenna",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Nursakina",
+      gender_ancestor: "female",
+      descendant: "Raski Adam Qaddafi",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Nursakina",
+      gender_ancestor: "female",
+      descendant: "Ubay Abiyyu Yusuf",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Nursakina",
+      gender_ancestor: "female",
+      descendant: "Syakira Assyifa Maryam",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Jenny Wari Jazz",
+      gender_ancestor: "female",
+      descendant: "Syagaz Al-Barra Zicetta Ra'uf",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Jenny Wari Jazz",
+      gender_ancestor: "female",
+      descendant: "Zylgan Diaz Abqary",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Meza Febriyanti",
+      gender_ancestor: "female",
+      descendant: "Hauzan",
+      gender_descendant: "male"
+    },
+    {
+      ancestor: "Maylani Usfa Putri",
+      gender_ancestor: "female",
+      descendant: "Dina ",
+      gender_descendant: "female"
+    },
+    {
+      ancestor: "Maylani Usfa Putri",
+      gender_ancestor: "female",
+      descendant: "Gibran",
+      gender_descendant: "male"
+    }
+];
